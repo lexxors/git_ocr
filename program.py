d5e920c7769e003580811b2bf05f690d7814ea67
lexxor <fgranowsky@gmail.com>
2023-11-27 18:38:18 +0300

    Программа номер 3

--- program.py
+++ program.py
@@ -1,8 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import Qt
-from PyQt5.uic import loadUi
 import random
 
 
@@ -14,27 +12,33 @@ class Circle:
         self.color = color
 
 
+class CircleDrawerUI(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setGeometry(100, 100, 300, 100)
+        self.setWindowTitle('Circle Drawer')
+        layout = QVBoxLayout()
+        self.pushButton = QPushButton('Добавить окружность', self)
+        layout.addWidget(self.pushButton)
+        self.setLayout(layout)
+
+
 class CircleDrawer(QMainWindow):
     def __init__(self):
         super().__init__()
         self.circles = []
-        loadUi('untitled.ui', self)
-        self.init_ui()
-
-    def init_ui(self):
-        self.setGeometry(100, 100, 800, 600)
-        self.setWindowTitle('Circle Drawer')
-        layout = QVBoxLayout()
-        self.pushButton.clicked.connect(self.add_circle)
-        layout.addWidget(self.pushButton)
-        self.setLayout(layout)
+        self.ui = CircleDrawerUI()
+        self.setCentralWidget(self.ui)
+        self.ui.pushButton.clicked.connect(self.add_circle)
 
     def add_circle(self):
         x = random.randint(50, 750)
         y = random.randint(50, 550)
         radius = random.randint(10, 50)
-        color = QColor(Qt.yellow)
-
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.circles.append(Circle(x, y, radius, color))
         self.repaint()
 
@@ -49,5 +53,7 @@ class CircleDrawer(QMainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = CircleDrawer()
+    window.setGeometry(100, 100, 800, 600)
+    window.setWindowTitle('Circle Drawer')
     window.show()
     sys.exit(app.exec_())
